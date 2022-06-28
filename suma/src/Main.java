dde050bff2bffe25971082ddcc418418ca22019c
Mauro86 <108375304+Mauro86@users.noreply.github.com>
2022-06-28 20:58:19 -0300

    Add files via upload

---
+++ suma/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a; int b; int c;
+        suma(a=10, b=20, c=30);
+    }
+    public static void suma(int a, int b, int c){
+        int resultado = a + b + c;
+
+        System.out.println(resultado);
+    }
+}
